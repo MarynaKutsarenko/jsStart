f0ed1a66dd9f4c4e6fb0d6d9208a79abbfc43416
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-01-24 21:45:52 +0200

    work with methods and properties

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+let money = 1200,
+    income = ' подработка ',
+    addExpenses = 'тренажерка, квартира, коммуналка, интернет',
+    deposit = true,
+    mission = 6000,
+    period = 6,
+    budgetDay = (money / 30);
+
+  console.log(typeof money);
+  console.log(typeof income);
+  console.log(typeof deposit);
+  console.log(addExpenses.length);
+  console.log('period : ' , period );
+  console.log('mission : ' , mission);
+  console.log(addExpenses.toLocaleLowerCase());
+  console.log(addExpenses.split(' '));
+  console.log(budgetDay);
+
 
   
 
-alert('My first project');
-console.log('Hello my teacher');
