377fa2faaffe71e0d366560341e8914632cb10aa
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-01-25 21:32:14 +0200

    done extra task

--- js/main.js
+++ js/main.js
@@ -1,11 +1,26 @@
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+let num = 266219,
+    result = [],
+    power,
+    arr = (num).toString().split('');
 
-  
+    for (let i = 0; i < arr.length; i++){
+        result.push(parseInt(arr[i]));   
+    };
 
-alert('My first project');
-console.log('Hello my teacher');
+    let mult = result.reduce(function (total, item) {
+      return total * item;
+    }, 1);
+
+    power = (mult ** 3);
+
+    function mat() {
+      let cut = (power.toString().substring(0, 2));
+      alert(cut);
+    }
+    mat()
+
+console.log(power);
+console.log(arr);
+console.log(result);
+console.log(mult);
+
