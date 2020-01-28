9d6a91ae8163b8c3185ae928398cae46363e81f3
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-01-28 09:11:55 +0200

    all about if else

--- js/main.js
+++ js/main.js
@@ -1,23 +1,56 @@
 'use strict'
 
-let money = 1200,
+let money = +prompt('Ваш месячный доход?'),
     income = ' подработка ',
-    addExpenses = 'тренажерка, квартира, коммуналка, интернет',
-    deposit = true,
-    mission = 6000,
-    period = 6,
-    budgetDay = (money / 30);
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'тренажерка, квартира, интернет') ,
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    depositResalt = deposit === true ? 'true' : 'false',
+    mission = 6000;
 
-  console.log(typeof money);
+function expenses() {
+  let questionSpending = prompt('Введите обязательную статью расходов!'),
+    questionCost = +prompt('Во сколько это обойдется?');
+  return{
+    questionSpending: questionSpending,
+    questionCost: questionCost
+  };
+}
+
+let resalt1 = expenses();
+let resalt2 = expenses();
+
+let budgetMonth = money - (resalt1.questionCost + resalt2.questionCost),
+    budgetDay = (budgetMonth / 30),
+    period = mission / budgetMonth;
+
+
+
+  switch (budgetDay) {
+    case  (budgetDay > 1200) :
+    case  (budgetDay = 1200) :
+      console.log('У вас высокий уровень дохода');
+      break;
+    case  (budgetDay > 600) : 
+    case  (budgetDay = 600) :
+      console.log('У вас средний уровень дохода');
+      break;
+    case  (budgetDay < 600) : console.log('К сожалению у вас уровень дохода ниже среднего');
+      break;
+    case  (budgetDay < 0) : console.log('Что то пошло не так');
+      break;
+    default: console.log('Не правильно работает');
+  }
+
+  
+  console.log(money);
+  console.log(depositResalt);
   console.log(typeof income);
-  console.log(typeof deposit);
+  console.log(deposit);
   console.log(addExpenses.length);
   console.log('Период равен ' + period + ' месяцев ');
   console.log('Цель заработать ' + mission + ' долларов');
   console.log(addExpenses.toLocaleLowerCase());
   console.log(addExpenses.split(', '));
-  console.log(budgetDay);
-
-
-  
-
+  console.log(budgetMonth);
+  console.log(Math.floor(budgetDay));
+  console.log(Math.ceil(period));
