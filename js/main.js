2e91c6954d332ce5d564143386fc9c46f23c62c3
Maryna Kutsarenko <m.kutsarenko@gmail.com>
2020-01-28 16:20:38 +0200

    all about if else , changed to 'if else'

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 'use strict'
 
-let money = +prompt('Ваш месячный доход?'),
+const money = +prompt('Ваш месячный доход?'),
+      deposit = confirm('Есть ли у вас депозит в банке?'),
+      depositResalt = deposit === true ? 'true' : 'false';
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'тренажерка, квартира, интернет') ,
     income = ' подработка ',
-    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'тренажерка, квартира, интернет') ,
-    deposit = confirm('Есть ли у вас депозит в банке?'),
-    depositResalt = deposit === true ? 'true' : 'false',
     mission = 6000;
 
 function expenses() {
-  let questionSpending = prompt('Введите обязательную статью расходов!'),
-    questionCost = +prompt('Во сколько это обойдется?');
+  const questionSpending = prompt('Введите обязательную статью расходов!', ' квартира'),
+        questionCost = +prompt('Во сколько это обойдется?', '4000');
   return{
     questionSpending: questionSpending,
-    questionCost: questionCost
+    questionCost: questionCost,
   };
 }
 
@@ -21,25 +21,19 @@ let resalt2 = expenses();
 
 let budgetMonth = money - (resalt1.questionCost + resalt2.questionCost),
     budgetDay = (budgetMonth / 30),
-    period = mission / budgetMonth;
+    period = (mission / budgetMonth);
 
+if (budgetDay >= 1200)
+  console.log('У вас высокий уровень дохода');
+else if (budgetDay >= 600 && budgetDay < 1200)
+  console.log('У вас средний уровень дохода');
+else if (budgetDay < 600)
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+else (budgetDay = 0 && budgetDay < 0) 
+  console.log('Что то пошло не так');
+ 
 
-
-  switch (true) {
-    case  (budgetDay > 1200) :
-    case  (budgetDay = 1200) :
-      console.log('У вас высокий уровень дохода');
-      break;
-    case  (budgetDay > 600) : 
-    case  (budgetDay = 600) :
-      console.log('У вас средний уровень дохода');
-      break;
-    case  (budgetDay < 600) : console.log('К сожалению у вас уровень дохода ниже среднего');
-      break;
-    case  (budgetDay < 0) : console.log('Что то пошло не так');
-      break;
-    default: console.log('Не правильно работает');
-  }
+ 
 
   console.log(money);
   console.log(depositResalt);
